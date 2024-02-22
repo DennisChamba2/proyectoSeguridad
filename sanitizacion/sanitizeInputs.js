e04b6bb8631aedd65c09f53812175c2cab67ae16
Wellinton Peralta <99459961+WellintonPeralta@users.noreply.github.com>
2024-02-22 00:22:39 -0500

    Update sanitizeInputs.js

--- sanitizacion/sanitizeInputs.js
+++ sanitizacion/sanitizeInputs.js
@@ -1,15 +1,37 @@
-//sanitizacion otros campos
+//sanitizacion para credenciales
 const { blacklist } = require('validator');
+
+// Función para sanitizar los datos de entrada del username
+function sanitizeUser(input) {
+
+    // Eliminamos los caracteres que no hemos definido
+    const sanitizedInput = input.replace(/[^a-zA-Z0-9\s@.]/g, '');
+
+    // Limitar la longitud máxima a 15 caracteres
+    const maxLength = 15;
+    const truncatedInput = sanitizedInput.slice(0, maxLength);
+
+    return truncatedInput;
+}
+// Sanitizar datos de la contrasena
+function sanitizePassword(input) {
+
+    // Eliminamos los caracteres que no hemos definido
+    const sanitizedInput = input.replace(/[^a-zA-Z0-9\s@.]/g, '');
+
+    // Limitar la longitud maxima
+    const maxLength = 15;
+    const truncatedInput = sanitizedInput.slice(0, maxLength);
+
+    return truncatedInput;
+}
 
 // Función para sanitizar los datos de entrada
 function sanitizeInput(input) {
 
-    // Eliminar caracteres especiales y espacios en blanco
+    // Eliminamos los caracteres que no hemos definido
     const sanitizedInput = input.replace(/[^a-zA-Z0-9\s]/g, '');
-
 
     return sanitizedInput;
 }
-
-
-module.exports = sanitizeInput;
+module.exports = { sanitizeUser, sanitizePassword, sanitizeInput };
